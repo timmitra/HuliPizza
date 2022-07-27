1934499ae849bf9b9f631bd9679715e195b4b44b
Tim Mitra <tim@it-guy.com>
2022-07-27 01:08:32 -0400

    images, effects

--- HuliPizza/Menu View/MenuRowView.swift
+++ HuliPizza/Menu View/MenuRowView.swift
@@ -11,16 +11,21 @@ struct MenuRowView: View {
   var body: some View {
     HStack(alignment: .top, spacing: 15) {
       Image("1_100w")
+//        .border(Color("G4"), width:2)
+//        .cornerRadius(10)
+        .clipShape(Capsule())
+        .shadow(color: Color.black.opacity(0.5),  radius: 5, x: 5, y: 5)
       VStack(alignment: .leading) {
         Text("Huli Chicken Pizza")
-          .font(.title)
+            .font(.title)
           .fontWeight(.light)
         RatingsView()
+      }
       }
       Spacer()
     }
   }
-}
+
 
 struct MenuRowView_Previews: PreviewProvider {
     static var previews: some View {
