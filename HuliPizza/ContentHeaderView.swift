be99ca57f2805e851f47232b944a19ced1245eff
Tim Mitra <tim@it-guy.com>
2022-07-28 00:33:11 -0400

    scaling modifier

--- HuliPizza/ContentHeaderView.swift
+++ HuliPizza/ContentHeaderView.swift
@@ -14,6 +14,8 @@ struct ContentHeaderView: View {
           Image("Surf Board")
             .resizable()
             .scaledToFit()
+//            .scaledToFill()
+            .frame(minWidth: 300, idealWidth: 500, maxWidth: 600, minHeight: 75, idealHeight: 143, maxHeight: 150, alignment: .center)
           Text("Huli Pizza Company")
             .font(Font.custom("Avinir-Black", size: 20))
             .foregroundColor(.white)
