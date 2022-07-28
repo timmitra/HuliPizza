9199dc04e025cdc9d0e98e99534f485d7d003744
Tim Mitra <tim@it-guy.com>
2022-07-28 00:24:49 -0400

    layoutPriority

--- HuliPizza/ContentView.swift
+++ HuliPizza/ContentView.swift
@@ -12,9 +12,12 @@ struct ContentView: View {
         
       VStack {
         ContentHeaderView()
+          .layoutPriority(2)
         PageTitleView(title: "Order Pizza")
         MenuListView()
+          .layoutPriority(1)
         OrderListView()
+          .layoutPriority(1)
         Spacer()
       }
       .padding()
